bb0a70c4b8a816081179777ca302cb265e1ebde0
zhsama <a602693793@gmail.com>
2025-01-20 10:45:18 +0800

    chore: update version to 0.1.7

--- linuxdo.js
+++ linuxdo.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         linuxdo保活
 // @namespace    http://tampermonkey.net/
-// @version      0.1.6
+// @version      0.1.7
 // @description  linuxdo自动浏览帖子，自动点赞
 // @author       zhcf1ess
 // @match        https://linux.do/*
